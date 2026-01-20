ab262d78cb013229a0414b20d48fa8d8258fbe1d
Muhammadamin <sharifjanovmukhammadamin@gmail.com>
2026-01-20 16:43:52 +0500

    chore: add deployment command and update CORS origins for backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -21,6 +21,8 @@ app.use(
       "http://localhost:5173",
       "http://localhost:4173",
       "https://beproductive.vercel.app",
+      "https://beproductive.muhammadamin.tech",
+      "https://darling-peony-4e940f.netlify.app"
     ],
     credentials: true,
   }),
